f1972639070ce947d0768d2f0e4b8a0155b84c7c
sfmikebrophy <40724682+sfmikebrophy@users.noreply.github.com>
2018-07-16 23:03:42 -0700

    Add files via upload

--- ProjectWilson.ino
+++ ProjectWilson.ino
@@ -11,7 +11,6 @@
 /**********************************/
 // TODO
 /**********************************
- * Add photoelectric sensor code
  * Add SatModem code
  * Add Data Package code
  * Add Fault Tolerance code
@@ -34,7 +33,6 @@
 #include <SoftwareSerial.h>
 #include <EEPROM.h>
 #include <HardwareSerial.h>
-#include <Sleep_n0m1.h>
 #include <OneWire.h>
 #include <DallasTemperature.h> //Temp sensors
 #include <Wire.h>
@@ -43,6 +41,10 @@
 #include <TinyGPS++.h> //GPS
 #include <RtcDS1307.h> //real-time clock
 #include <Adafruit_MPL115A2.h> //Barometric sensor
+#include <avr/wdt.h>            // library for default watchdog functions
+#include <avr/interrupt.h>      // library for interrupts handling
+#include <avr/sleep.h>          // library for sleep
+#include <avr/power.h>          // library for power control
 
 /*****************************/
 // DEFINE PIN ASSIGNMENTS
@@ -67,12 +69,7 @@ SoftwareSerial gpsSS(11, 50); //used for GPS
 // Instantiate the HUMIDITY & TEMP SENSOR object
 SHT1x sht1x(dataPin, sckPin);
 
-// Instantiate the SLEEP object
-Sleep sleep;
 RtcDS1307<TwoWire> Rtc(Wire);
-
-//Callibrated Clock Cycle 10 minute timer
-unsigned long sleepTime = 603500;
 
 OneWire oneWireTemp1(TEMP1_WIRE_BUS);
 OneWire oneWireTemp2(TEMP2_WIRE_BUS);
@@ -88,26 +85,28 @@ IridiumSBD modem(IridiumSerial); //SatModem
 // EEPROM Address Pool
 const int eelat = 0;
 const int eelng = 4;
-
 const int eemonth = 8;
 const int eeday = 10;
 const int eeyear = 12;
-
 const int eehour = 14;
 const int eeminute = 16;
-
 const int ee_extAirTemp = 18;
 const int ee_waterTemp = 22;
 const int ee_intAirTemp = 26;
-
 const int ee_humidity = 30;
-
 const int ee_barometric = 34;
-
 const int ee_lightIntensity = 38;
+
 /*****************************/
 // DEFINE VARS
 /*****************************/
+// how many times remain to sleep before wake up
+// volatile to be modified in interrupt function
+volatile int nbr_remaining; 
+
+// pin on which a led is attached on the board
+#define led 13
+
 //If Already Run Routine Check
 boolean if_0hr_routine = false;
 boolean if_12hr_routine = false;
@@ -145,20 +144,43 @@ int err;
 
 
 void setup()
-{
+{  
+  //watchdog and sleep functionality
+  // use led 13 and put it in low mode
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  
+  delay(1000);
+  
+  // configure the watchdog
+  configure_wdt();
+
   Serial.begin(115200); //initiate serial comm with PC
   gpsSS.begin(9600); //initiate serial comm with GPS
   tempExtAir.begin();
   tempWater.begin();
   //initiate humidity sensor comm here
   Rtc.Begin(); //initiate serial comm with real-time clock
+  
+  gpsAOS(); //acquire GPS signal and time
+  syncRTCTimeToGPS(); //sync RTC to time from GPS
+  delay(500);
+  
   barometer.begin();
   //IridiumSerial.begin(19200); //initiate serial comm with SatModem
 
   Serial.println("Starting up...");
   delay(100);
-  
-  //SYSTEMS TESTING
+}
+
+void loop()
+{  
+  // sleep for a given number of cycles in lowest power mode, each cycle = 8 secs
+  sleep(1); //run every 10 minutes, which is 75 cycles
+  wakeup(); //time check
+
+  // testing only
+  /*
   gpsAOS();
   syncRTCTimeToGPS();
   fetchExtAirTemp();
@@ -168,27 +190,12 @@ void setup()
   fetchBarometric();
   fetchLightSensorData();
   printOutEEPROM();
-}
-
-void loop()
-{  
-  /*****************************/
-  /* Sleep Clock Routine
-  /*****************************/
-
-/*
-
-  Serial.println("Going to Sleep...");
-  RtcDateTime now = Rtc.GetDateTime();
-  printDateTime(now);
-  Serial.println();
-  delay(50);
-  sleep.pwrDownMode(); //set sleep mode
-  sleep.sleepDelay(sleepTime); //sleep for: sleepTime
-  Serial.println("Waking up...");
-  wakeup();
-
-*/
+  */
+  
+  // now a real hang is happening: this will rebood the board
+  // after 8 seconds
+  // (at the end of the sleep, nrb_remaining = 0)
+  //while (1); //use for hang testing only
   
 }
 
@@ -213,8 +220,11 @@ void wakeup()
   /*****************************/
   /* 0 Hour Routine Check
   /*****************************/
-  
-  if(/*rtc_hour == 0 && */rtc_minute >= 0 && rtc_minute <= 30 && if_0hr_routine == false)
+  RtcDateTime now = Rtc.GetDateTime();
+  printDateTime(now);
+  Serial.println();
+  delay(100);
+  if(rtc_hour == 6 && if_0hr_routine == false)
   {
     hr0_routine();
     if_0hr_routine = true;
@@ -225,7 +235,7 @@ void wakeup()
   /* 12 Hour Routine Check
   /*****************************/
 
-  if(/*rtc_hour == 12 && */rtc_minute >= 0 && rtc_minute <= 30 && if_12hr_routine == false)
+  if(rtc_hour == 12 && if_12hr_routine == false)
   {
     hr12_routine();
     if_12hr_routine = true;
@@ -255,6 +265,7 @@ void hr0_routine()
   fetchIntAirTemp();
   fetchHumidity();
   fetchBarometric();
+  fetchLightSensorData();
   printOutEEPROM();
   
 }
@@ -278,6 +289,7 @@ void hr12_routine()
   fetchIntAirTemp();
   fetchHumidity();
   fetchBarometric();
+  fetchLightSensorData();
   printOutEEPROM();
   
 }
@@ -428,8 +440,7 @@ void fetchLightSensorData()
 {
   // read the value from the photosensor
   p_lightIntensity = analogRead(lightSensorPin);
-  Serial.print(p_lightIntensity);
-  Serial.println();
+
   // Storing to EEPROM
   EEPROM.put(ee_lightIntensity, p_lightIntensity);
 }
@@ -480,6 +491,92 @@ float convertTemp(float cTemp){
   return fTemp;
 }
 
+
+// interrupt raised by the watchdog firing
+// when the watchdog fires, this function will be executed
+// remember that interrupts are disabled in ISR functions
+// now we must check if the board is sleeping or if this is a genuine
+// interrupt (hanging)
+ISR(WDT_vect)
+{
+    // Check if we are in sleep mode or it is a genuine WDR.
+    if(nbr_remaining > 0)
+    {
+        // not hang out, just waiting
+        // decrease the number of remaining avail loops
+        nbr_remaining = nbr_remaining - 1;
+        wdt_reset();
+    }
+    else
+    {
+        // must be rebooted
+        // configure
+        MCUSR = 0;                          // reset flags
+       
+                                            // Put timer in reset-only mode:
+        WDTCSR |= 0b00011000;               // Enter config mode.
+        WDTCSR =  0b00001000 | 0b000000;    // clr WDIE (interrupt enable...7th from left)
+                                            // set WDE (reset enable...4th from left), and set delay interval
+                                            // reset system in 16 ms...
+                                            // unless wdt_disable() in loop() is reached first
+                                       
+        // reboot
+        while(1);
+    }
+}
+
+
+// function to configure the watchdog: let it sleep 8 seconds before firing
+// when firing, configure it for resuming program execution by default
+// hangs will correspond to watchdog fired when nbr_remaining <= 0 and will
+// be determined in the ISR function
+void configure_wdt(void)
+{
+ 
+  cli();                           // disable interrupts for changing the registers
+
+  MCUSR = 0;                       // reset status register flags
+
+                                   // Put timer in interrupt-only mode:                                       
+  WDTCSR |= 0b00011000;            // Set WDCE (5th from left) and WDE (4th from left) to enter config mode,
+                                   // using bitwise OR assignment (leaves other bits unchanged).
+  WDTCSR =  0b01000000 | 0b100001; // set WDIE: interrupt enabled
+                                   // clr WDE: reset disabled
+                                   // and set delay interval (right side of bar) to 8 seconds
+
+  sei();                           // re-enable interrupts 
+}
+
+// Put the Arduino to deep sleep. Only an interrupt can wake it up.
+void sleep(int ncycles)
+{  
+  nbr_remaining = ncycles; // defines how many cycles should sleep
+
+  // Set sleep to full power down.  Only external interrupts or
+  // the watchdog timer can wake the CPU!
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+ 
+  // Turn off the ADC while asleep.
+  power_adc_disable();
+ 
+  while (nbr_remaining > 0){ // while some cycles left, sleep!
+
+  // Enable sleep and enter sleep mode.
+  sleep_mode();
+
+  // CPU is now asleep and program execution completely halts!
+  // Once awake, execution will resume at this point if the
+  // watchdog is configured for resume rather than restart
+ 
+  // When awake, disable sleep mode
+  sleep_disable();
+ 
+  }
+ 
+  // put everything on again
+  power_all_enable();
+ 
+}
 
 
 
